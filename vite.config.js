fb7dba890ef46ee7792553af15207fe1c24ccace
Leileisme <onlymymind1108@gmail.com>
2024-01-19 16:37:33 +0800

    Google分析/PWA ing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+// 引入 vite 的 google 分析打包工具(引入方式依文件)
+import { VitePluginRadar } from 'vite-plugin-radar'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -25,7 +28,31 @@ export default defineConfig({
           styles: 'wght@100;300;400;500;700;900'
         }]
       }
+    }),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-X57R0M4Q2L'
+      }
+    }),
+    VitePWA({
+      // 設定快取自動更新
+      registerType: 'autoUpdate',
+      // Service Worker 產生設定
+      workbox: {
+        // 清除過期快取
+        cleanupOutdatedCaches: true,
+        // 快取檔案路徑
+        globPatterns: [
+          // 任意資料夾內任意檔名，符合這些的副檔名檔案
+          '**/*.{html,css,js,woff,eot,ttf,woff2,ico,jpg,jpeg,png,svg,gif}**'
+        ],
+        // 忽略網址參數，預設不會快取有參數的檔案或頁面
+        ignoreURLParametersMatching: [/.*/]
+      }
+
     })
+
   ],
   define: { 'process.env': {} },
   resolve: {
